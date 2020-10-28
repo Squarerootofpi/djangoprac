5754e253fd2427616798624e73399b5a8bf8fb91
Squarerootofpi <53194917+Squarerootofpi@users.noreply.github.com>
2020-10-27 19:28:45 -0600

    Adding dynamic html to file.
    Done!

--- homepage/views.py
+++ homepage/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def indexPageView(request) :
-    return HttpResponse('Hello Universe!')
+    sOutput = '<html><head><title>My Title</title></head><body><p style="color:red;"><b>one</b></p><p style="color:blue;">two</p><p style="font-size:50px;">three</p><ul><li>A</li><li>B</li><li>C</li></ul></body></html>'
+    return HttpResponse(sOutput)
